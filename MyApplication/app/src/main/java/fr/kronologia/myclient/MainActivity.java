70ca7dcf801c1ef64545eaa415742966f68041cb
Maxence Vialla <maxence.vialla@gmail.com>
2017-04-12 14:20:46 +0200

    end android app

--- MyApplication/app/src/main/java/fr/kronologia/myclient/MainActivity.java
+++ MyApplication/app/src/main/java/fr/kronologia/myclient/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.util.Log;
 
@@ -26,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
 
     RequestQueue queue;
     TextView myTV;
+    EditText myET;
+    EditText idET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,33 +38,44 @@ public class MainActivity extends AppCompatActivity {
         queue = Volley.newRequestQueue(this);
 
         myTV = (TextView) findViewById(R.id.text);
+        myET = (EditText) findViewById(R.id.nameET);
+        idET = (EditText) findViewById(R.id.idET);
         final Button getAllButton = (Button) findViewById(R.id.getAllButton);
         final Button putButton = (Button) findViewById(R.id.addButton);
+        final Button getButton = (Button) findViewById(R.id.getButton);
 
         getAllButton.setOnClickListener(getAllListener);
         putButton.setOnClickListener(addListener);
+        getButton.setOnClickListener(getListener);
 
-// Instantiate the RequestQueue.
 
-        String url ="http://10.21.135.243:8080/myapp/character/51";
-
-// Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        myTV.setText("Response is: "+ response);
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                myTV.setText("That didn't work!");
-            }
-        });
-// Add the request to the RequestQueue.
-        queue.add(stringRequest);
 
     }
+
+    View.OnClickListener getListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            final String id = idET.getText().toString();
+            String url ="http://10.21.135.243:8080/myapp/character/" + id;
+
+// Request a string response from the provided URL.
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            myTV.setText("Get character identified by id " + id + " : "+ response);
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    myTV.setText("That didn't work!");
+                }
+            });
+// Add the request to the RequestQueue.
+            queue.add(stringRequest);
+        }
+    };
 
     View.OnClickListener getAllListener = new View.OnClickListener() {
         @Override
@@ -91,13 +105,14 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v) {
             try {
                 final String url = "http://10.21.135.243:8080/myapp/character";
-                final JSONObject jsonBody = new JSONObject("{\"name\":\"paul\",\"level\":\"1\"}");
+                final String myName = myET.getText().toString();
+                final JSONObject jsonBody = new JSONObject("{\"name\":\"" + myName + "\",\"level\":\"1\"}");
 
                 JsonObjectRequest request = new JsonObjectRequest(url, jsonBody, new Response.Listener<JSONObject>() {
 
                     @Override
                     public void onResponse(JSONObject response) {
-                        myTV.setText("prout");
+                        myTV.setText("Added a new character named " + myName + " !");
                     }
                 }, new Response.ErrorListener() {
                     @Override
